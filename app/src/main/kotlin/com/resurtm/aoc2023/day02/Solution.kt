20661b366d78eea77536c7612127f74c1840a4ea
resurtm <resurtm@gmail.com>
2023-12-02 16:08:25 +0100

    Day 02 solution. Part 2.

--- app/src/main/kotlin/com/resurtm/aoc2023/day02/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day02/Solution.kt
@@ -1,43 +1,85 @@
 package com.resurtm.aoc2023.day02
 
 fun launchDay02(testCase: String) {
-    var result = 0
+    var resultPart1 = 0
+    var resultPart2 = 0
     val reader = object {}.javaClass.getResourceAsStream(testCase)?.bufferedReader() ?: return
     while (true) {
         val res = parseLine(reader.readLine() ?: break)
-        if (res.second) {
-            result += res.first
+        if (res.possible) {
+            resultPart1 += res.gameNum
         }
+        resultPart2 += res.power
     }
-    println("Part 1 answer: $result")
+    println("Part 1 answer: $resultPart1")
+    println("Part 2 answer: $resultPart2")
 }
 
-private fun parseLine(line: String): Pair<Int, Boolean> {
+private fun parseLine(line: String): ParseLineResult {
     val parts = line.split(":")
     val gameNum = parts[0].trim().split(" ")[1].trim().toInt()
+
+    var possible = true
+    var maxRed = -1
+    var maxGreen = -1
+    var maxBlue = -1
+
     for (reveal in parts[1].trim().split(";")) {
         var red = 12
         var green = 13
         var blue = 14
+
+        var redAccum = 0
+        var greenAccum = 0
+        var blueAccum = 0
+
         for (rawCubes in reveal.trim().split(",")) {
             val cubes = rawCubes.trim().split(" ")
+            val value = cubes[0].trim().toInt()
+
             when (cubes[1]) {
                 "red" -> {
-                    red -= cubes[0].toInt()
-                    if (red < 0) return Pair(gameNum, false)
+                    redAccum += value
+                    red -= value
+                    if (red < 0) {
+                        possible = false
+                    }
                 }
 
                 "green" -> {
-                    green -= cubes[0].toInt()
-                    if (green < 0) return Pair(gameNum, false)
+                    greenAccum += value
+                    green -= value
+                    if (green < 0) {
+                        possible = false
+                    }
                 }
 
                 "blue" -> {
-                    blue -= cubes[0].toInt()
-                    if (blue < 0) return Pair(gameNum, false)
+                    blueAccum += value
+                    blue -= value
+                    if (blue < 0) {
+                        possible = false
+                    }
                 }
             }
         }
+
+        if (maxRed < redAccum) {
+            maxRed = redAccum
+        }
+        if (maxBlue < blueAccum) {
+            maxBlue = blueAccum
+        }
+        if (maxGreen < greenAccum) {
+            maxGreen = greenAccum
+        }
     }
-    return Pair(gameNum, true)
+
+    return ParseLineResult(gameNum, possible, maxRed * maxBlue * maxGreen)
 }
+
+private data class ParseLineResult(
+    val gameNum: Int,
+    val possible: Boolean,
+    val power: Int,
+)
