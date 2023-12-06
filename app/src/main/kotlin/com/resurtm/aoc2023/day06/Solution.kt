2ff14dd7c157dbe8c7aaeb941897c6d9a4bcf86a
resurtm <resurtm@gmail.com>
2023-12-06 10:14:14 +0100

    Day 06, part 2.

--- app/src/main/kotlin/com/resurtm/aoc2023/day06/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day06/Solution.kt
@@ -3,12 +3,19 @@ package com.resurtm.aoc2023.day06
 fun launchDay06(testCase: String) {
     val inputData = readInputData(testCase)
     println("Day 06, part 1: ${calcPart1(inputData)}")
+    println("Day 06, part 2: ${calcPart2(inputData)}")
 }
 
-private fun calcPart1(inp: InputData): Int {
-    var result = 1
+private fun calcPart2(inp: InputData): Long {
+    val source = inp.map { it.first }.joinToString("").toLong()
+    val goal = inp.map { it.second }.joinToString("").toLong()
+    return calcPart1(listOf(Pair(source, goal)))
+}
+
+private fun calcPart1(inp: InputData): Long {
+    var result = 1L
     for (race in inp) {
-        var ways = 0
+        var ways = 0L
         for (hold in 0..race.first) {
             val distance = (race.first - hold) * hold
             if (distance > 0 && distance > race.second) ways++
@@ -28,7 +35,7 @@ private fun readInputData(testCase: String): InputData {
     return prepLine(timeLine) zip prepLine(distLine)
 }
 
-private fun prepLine(line: String): List<Int> =
-    line.split(":")[1].trim().split(" ").map { it.trim() }.filter { it.isNotEmpty() }.map { it.toInt() }
+private fun prepLine(line: String): List<Long> =
+    line.split(":")[1].trim().split(" ").map { it.trim() }.filter { it.isNotEmpty() }.map { it.toLong() }
 
-typealias InputData = List<Pair<Int, Int>>
+typealias InputData = List<Pair<Long, Long>>
