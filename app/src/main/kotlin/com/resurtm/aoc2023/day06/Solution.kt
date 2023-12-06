c07c9aafc995baba4b053f91bb79f6605c55b767
resurtm <resurtm@gmail.com>
2023-12-06 10:04:19 +0100

    Day 06, part 1.

--- app/src/main/kotlin/com/resurtm/aoc2023/day06/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day06/Solution.kt
@@ -1,10 +1,24 @@
 package com.resurtm.aoc2023.day06
 
 fun launchDay06(testCase: String) {
-    println(readInputData(testCase))
+    val inputData = readInputData(testCase)
+    println("Day 06, part 1: ${calcPart1(inputData)}")
 }
 
-private fun readInputData(testCase: String): List<Pair<Int, Int>> {
+private fun calcPart1(inp: InputData): Int {
+    var result = 1
+    for (race in inp) {
+        var ways = 0
+        for (hold in 0..race.first) {
+            val distance = (race.first - hold) * hold
+            if (distance > 0 && distance > race.second) ways++
+        }
+        result *= ways
+    }
+    return result
+}
+
+private fun readInputData(testCase: String): InputData {
     val ex = Exception("Cannot read the input")
     val rawReader = object {}.javaClass.getResourceAsStream(testCase)?.bufferedReader()
     val reader = rawReader ?: throw ex
@@ -16,3 +30,5 @@ private fun readInputData(testCase: String): List<Pair<Int, Int>> {
 
 private fun prepLine(line: String): List<Int> =
     line.split(":")[1].trim().split(" ").map { it.trim() }.filter { it.isNotEmpty() }.map { it.toInt() }
+
+typealias InputData = List<Pair<Int, Int>>
