03e4a89a4d295de1b5a51108a895deb3579561dc
resurtm <resurtm@gmail.com>
2023-12-08 10:35:49 +0100

    Day 08, part 2, final solution

--- app/src/main/kotlin/com/resurtm/aoc2023/day08/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day08/Solution.kt
@@ -3,24 +3,33 @@ package com.resurtm.aoc2023.day08
 fun launchDay08(testCase: String) {
     val input = readInput(testCase)
     println("Day 08, part 1: ${solvePart1(input)}")
+    println("Day 08, part 2: ${solvePart2(input)}")
 }
 
-private fun solvePart1(input: Input): Int {
+private fun solvePart1(inp: Input): Long = calculate(inp)
+
+private fun solvePart2(inp: Input): Long {
+    val result = inp.points.keys.filter { it.last() == 'A' }.map { calculate(inp, start = it, end = "Z") }
+    return findListLCM(result)
+}
+
+private fun calculate(inp: Input, start: String = "AAA", end: String = "ZZZ"): Long {
     var currTurn = 0
-    var steps = 0
-    var point = "AAA"
+    var steps = 0L
+    var point = start
 
     do {
-        if (currTurn == input.turns.length) currTurn = 0
-        val nextPoint = input.points[point]
+        if (currTurn == inp.turns.length) currTurn = 0
+
+        val nextPoint = inp.points[point]
         if (nextPoint != null) {
-            if (input.turns[currTurn] == 'L') point = nextPoint.first
-            else if (input.turns[currTurn] == 'R') point = nextPoint.second
+            if (inp.turns[currTurn] == 'L') point = nextPoint.first
+            else if (inp.turns[currTurn] == 'R') point = nextPoint.second
         }
 
         currTurn++
         steps++
-    } while (point != "ZZZ")
+    } while (!point.endsWith(end))
 
     return steps
 }
@@ -49,3 +58,24 @@ private fun readInput(testCase: String): Input {
 }
 
 private data class Input(val turns: String, val points: MutableMap<String, Pair<String, String>>)
+
+private fun findListLCM(numbers: List<Long>): Long {
+    var result = numbers[0]
+    for (i in 1 until numbers.size) {
+        result = findItemLCM(result, numbers[i])
+    }
+    return result
+}
+
+private fun findItemLCM(a: Long, b: Long): Long {
+    val larger = if (a > b) a else b
+    val maxLcm = a * b
+    var lcm = larger
+    while (lcm <= maxLcm) {
+        if (lcm % a == 0L && lcm % b == 0L) {
+            return lcm
+        }
+        lcm += larger
+    }
+    return maxLcm
+}
