6ce72e8bfd8d09f7df7c7f73411fa92170d7f1bf
resurtm <resurtm@gmail.com>
2023-12-08 10:02:22 +0100

    Day 08, part 1, refactor

--- app/src/main/kotlin/com/resurtm/aoc2023/day08/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day08/Solution.kt
@@ -1,6 +1,31 @@
 package com.resurtm.aoc2023.day08
 
 fun launchDay08(testCase: String) {
+    val input = readInput(testCase)
+    println("Day 08, part 1: ${solvePart1(input)}")
+}
+
+private fun solvePart1(input: Input): Int {
+    var currTurn = 0
+    var steps = 0
+    var point = "AAA"
+
+    do {
+        if (currTurn == input.turns.length) currTurn = 0
+        val nextPoint = input.points[point]
+        if (nextPoint != null) {
+            if (input.turns[currTurn] == 'L') point = nextPoint.first
+            else if (input.turns[currTurn] == 'R') point = nextPoint.second
+        }
+
+        currTurn++
+        steps++
+    } while (point != "ZZZ")
+
+    return steps
+}
+
+private fun readInput(testCase: String): Input {
     val ex = Exception("Cannot read the input")
     val rawReader = object {}.javaClass.getResourceAsStream(testCase)?.bufferedReader()
     val reader = rawReader ?: throw ex
@@ -20,21 +45,7 @@ fun launchDay08(testCase: String) {
         points[key] = value
     }
 
-    var currTurn = 0
-    var steps = 0
-    var point = "AAA"
-
-    do {
-        if (currTurn == turns.length) currTurn = 0
-        val nextPoint = points[point]
-        if (nextPoint != null) {
-            if (turns[currTurn] == 'L') point = nextPoint.first
-            else if (turns[currTurn] == 'R') point = nextPoint.second
-        }
-
-        currTurn++
-        steps++
-    } while (point != "ZZZ")
-
-    println("Day 08, part 1: $steps")
+    return Input(turns = turns, points = points)
 }
+
+private data class Input(val turns: String, val points: MutableMap<String, Pair<String, String>>)
