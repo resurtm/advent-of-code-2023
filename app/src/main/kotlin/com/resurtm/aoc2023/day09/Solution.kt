52c9a2125953a2fe27dd0c0221215f1672538adb
resurtm <resurtm@gmail.com>
2023-12-09 09:36:33 +0100

    Day 09, part 2

--- app/src/main/kotlin/com/resurtm/aoc2023/day09/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day09/Solution.kt
@@ -1,27 +1,31 @@
 package com.resurtm.aoc2023.day09
 
 fun launchDay09(testCase: String) {
-    val ex = Exception("Cannot read the input")
-    val rawReader = object {}.javaClass.getResourceAsStream(testCase)?.bufferedReader()
-    val reader = rawReader ?: throw ex
-
+    val reader =
+        object {}.javaClass.getResourceAsStream(testCase)?.bufferedReader()
+            ?: throw Exception("Cannot read the input")
     var res1 = 0
+    var res2 = 0
     while (true) {
-        res1 += parseLine(reader.readLine() ?: break)
+        val res = parseLine(reader.readLine() ?: break)
+        res1 += res.first
+        res2 += res.second
     }
     println("Day 09, part 1: $res1")
+    println("Day 09, part 2: $res2")
 }
 
-private fun parseLine(line: String): Int {
+private fun parseLine(line: String): Pair<Int, Int> {
     var items = line.split(' ').map { it.trim() }.filter { it.isNotEmpty() }.map { it.toInt() }
     val accum = mutableListOf<Int>()
+    val accumV2 = mutableListOf<Int>()
     while (!items.all { it == 0 }) {
         accum.add(items.last())
+        accumV2.add(items.first())
         val nextItems = mutableListOf<Int>()
-        for (idx in 0..items.size - 2) {
+        for (idx in 0..items.size - 2)
             nextItems.add(items[idx + 1] - items[idx])
-        }
         items = nextItems
     }
-    return accum.sum()
+    return Pair(accum.sum(), accumV2.reversed().reduce { acc, i -> i - acc })
 }
