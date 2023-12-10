b0bf154ac70a7230e7f7fb8337d11aa93380f810
resurtm <resurtm@gmail.com>
2023-12-10 10:35:27 +0100

    Day 10, data reader

--- app/src/main/kotlin/com/resurtm/aoc2023/day10/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day10/Solution.kt
@@ -1,9 +1,11 @@
 package com.resurtm.aoc2023.day10
 
 fun launchDay10(testCase: String) {
-    val input = readInput(testCase)
-    println(input.pos)
-    for (row in input.grid) {
+    val env = readEnv(testCase)
+    ensureStart(env)
+
+    println(env.pos)
+    for (row in env.grid) {
         for (item in row) {
             print("${item.v} ")
         }
@@ -11,11 +13,41 @@ fun launchDay10(testCase: String) {
     }
 }
 
-private fun readInput(testCase: String): Input {
+private fun ensureStart(e: Env) {
+    val north = if (e.pos.row == 0) Pipe.BLANK else e.grid[e.pos.row][e.pos.col]
+    val south = if (e.pos.row == e.grid.size - 1) Pipe.BLANK else e.grid[e.pos.row][e.pos.col]
+
+    val west = if (e.pos.col == 0) Pipe.BLANK else e.grid[e.pos.row][e.pos.col]
+    val east = if (e.pos.col == e.grid[0].size) Pipe.BLANK else e.grid[e.pos.row][e.pos.col]
+
+    val newStart = ensureStartEx(north, south, west, east)
+    e.grid[e.pos.row][e.pos.col] = newStart
+}
+
+private fun ensureStartEx(north: Pipe, south: Pipe, west: Pipe, east: Pipe): Pipe {
+    val n = north in arrayOf(Pipe.NORTH_SOUTH, Pipe.SOUTH_WEST, Pipe.SOUTH_EAST)
+    val s = south in arrayOf(Pipe.NORTH_SOUTH, Pipe.NORTH_WEST, Pipe.NORTH_EAST)
+
+    val w = west in arrayOf(Pipe.WEST_EAST, Pipe.NORTH_EAST, Pipe.SOUTH_EAST)
+    val e = east in arrayOf(Pipe.WEST_EAST, Pipe.NORTH_WEST, Pipe.SOUTH_WEST)
+
+    if (n && s) return Pipe.NORTH_SOUTH
+    if (w && e) return Pipe.WEST_EAST
+
+    if (n && w) return Pipe.NORTH_WEST
+    if (n && e) return Pipe.NORTH_EAST
+
+    if (s && w) return Pipe.SOUTH_WEST
+    if (s && e) return Pipe.SOUTH_EAST
+
+    return Pipe.BLANK
+}
+
+private fun readEnv(testCase: String): Env {
     val reader =
         object {}.javaClass.getResourceAsStream(testCase)?.bufferedReader()
             ?: throw Exception("Cannot read the input")
-    val result = Input(pos = Pos(-1, -1))
+    val result = Env(pos = Pos(-1, -1))
     var row = 0
     while (true) {
         val rawLine = reader.readLine() ?: break
@@ -47,6 +79,6 @@ private enum class Pipe(val v: Byte) {
 
 private data class Pos(val row: Int = 0, val col: Int = 0)
 
-private data class Input(val grid: Grid = mutableListOf(), var pos: Pos = Pos())
+private data class Env(val grid: Grid = mutableListOf(), var pos: Pos = Pos())
 
 private typealias Grid = MutableList<MutableList<Pipe>>
