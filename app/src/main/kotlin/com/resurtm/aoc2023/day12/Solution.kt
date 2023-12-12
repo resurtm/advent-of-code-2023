8992997182885643bb7a723de85de3b18fcf0660
resurtm <resurtm@gmail.com>
2023-12-12 16:50:39 +0100

    Day 12, part 1, final

--- app/src/main/kotlin/com/resurtm/aoc2023/day12/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day12/Solution.kt
@@ -1,24 +1,56 @@
 package com.resurtm.aoc2023.day12
 
 fun launchDay12(testCase: String) {
-//    parseLine("?.? 1,1")
-//    parseLine("???.### 1,1,3")
-//     parseLine(".??..??...?##. 1,1,3")
-//     parseLine("?#?#?#?#?#?#?#? 1,3,1,6")
-//     parseLine("????.#...#... 4,1,1")
-    parseLine("?###???????? 3,2,1")
+    val reader =
+        object {}.javaClass.getResourceAsStream(testCase)?.bufferedReader()
+            ?: throw Exception("Invalid state, cannot read the input")
 
-//    val reader =
-//        object {}.javaClass.getResourceAsStream(testCase)?.bufferedReader()
-//            ?: throw Exception("Invalid state, cannot read the input")
-//
-//    while (true) {
-//        val rawLine = reader.readLine() ?: break
-//        parseLine(rawLine)
-//    }
+    var result = 0
+    while (true) {
+        val rawLine = reader.readLine() ?: break
+        result += parseLine(rawLine)
+    }
+    println("Day 12, part 1: $result")
 }
 
-private fun parseLine(rawLine: String) {
+private fun parseLine(rawLine: String): Int {
+    val parts = rawLine.split(' ')
+    val mask = parts[0].toMutableList()
+    val blocks = parts[1].split(',').map { it.trim() }.filter { it.isNotEmpty() }.map { it.toInt() }
+
+    val result = mutableSetOf<String>()
+    combine(mask, '#', blocks, result)
+    combine(mask, '.', blocks, result)
+    return result.size
+}
+
+private fun combine(mask: List<Char>, ch: Char, blocks: List<Int>, result: MutableSet<String>) {
+    val pos = mask.indexOf('?')
+    if (pos == -1) {
+        if (checkMask(mask, blocks)) result.add(mask.joinToString(""))
+        return
+    }
+    val nextMask = mask.subList(0, pos) + ch + mask.subList(pos + 1, mask.size)
+    combine(nextMask, '#', blocks, result)
+    combine(nextMask, '.', blocks, result)
+}
+
+private fun checkMask(mask: List<Char>, blocks: List<Int>): Boolean {
+    var block = 0
+    var accum = 0
+    (mask + '.').forEach {
+        if (it == '.') {
+            if (accum != 0) {
+                if (block == blocks.size || accum != blocks[block]) return false
+                block++
+            }
+            accum = 0
+        } else accum++
+    }
+    return block == blocks.size
+}
+
+private fun parseLineTemp(rawLine: String) {
     val parts = rawLine.split(' ')
     val mask = parts[0].toMutableList()
     val blocks = parts[1].split(',').map { it.trim() }.filter { it.isNotEmpty() }.map { it.toInt() }
