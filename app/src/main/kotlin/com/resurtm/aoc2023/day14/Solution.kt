8c246a88a13f3c2083f8bc76c97c10868c67fe61
resurtm <resurtm@gmail.com>
2023-12-14 07:34:10 +0100

    Day 14, part 1

--- app/src/main/kotlin/com/resurtm/aoc2023/day14/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day14/Solution.kt
@@ -2,7 +2,28 @@ package com.resurtm.aoc2023.day14
 
 fun launchDay14(testCase: String) {
     val input = readInput(testCase)
-    println(input)
+    for (row in input.indices) {
+        for (col in input[row].indices) {
+            if (input[row][col] == 'O') {
+                for (row2 in row - 1 downTo -1) {
+                    if (row2 == -1 || input[row2][col] != '.') {
+                        val x = input[row][col]
+                        input[row][col] = input[row2 + 1][col]
+                        input[row2 + 1][col] = x
+                        break
+                    }
+                }
+            }
+        }
+    }
+    var result = 0
+    var counter = input.size
+    input.forEach {
+        it.forEach { if (it == 'O') result += counter }
+        counter--
+    }
+    println(result)
+    // input.forEach { println(it.joinToString("")) }
 }
 
 private fun readInput(testCase: String): Input {
