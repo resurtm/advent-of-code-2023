35b0b32dcaadfbe1c72f6af27a2405ad36fe4494
resurtm <resurtm@gmail.com>
2023-12-15 09:49:56 +0100

    Day 15, part 2

--- app/src/main/kotlin/com/resurtm/aoc2023/day15/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day15/Solution.kt
@@ -2,8 +2,44 @@ package com.resurtm.aoc2023.day15
 
 fun launchDay15(testCase: String) {
     val input = readInput(testCase)
-    val part1 = input.sumOf { calcHash(it) }
-    println("Day 15, part 1: $part1")
+    println("Day 15, part 1: ${input.sumOf { calcHash(it) }}")
+    println("Day 15, part 2: ${calcPart2(input)}")
+}
+
+private fun calcPart2(items: List<String>): Int {
+    val boxes = mutableMapOf<Int, MutableList<Pair<String, Int>>>()
+    items.forEach { item ->
+        if (item.indexOf('=') != -1) {
+            val p = item.split('=')
+            val id = calcHash(p[0])
+            if (!boxes.containsKey(id)) boxes[id] = mutableListOf()
+            val box = boxes[id]
+            if (box != null) {
+                val pos = box.indexOfFirst { it.first == p[0] }
+                if (pos == -1)
+                    box.add(Pair(p[0], p[1].toInt()))
+                else
+                    box[pos] = Pair(p[0], p[1].toInt())
+            }
+        } else if (item.indexOf('-') != -1) {
+            val p = item.split('-')
+            val id = calcHash(p[0])
+            if (!boxes.containsKey(id)) boxes[id] = mutableListOf()
+            val box = boxes[id]
+            if (box != null) {
+                val pos = box.indexOfFirst { it.first == p[0] }
+                if (pos != -1)
+                    boxes[id] = (box.subList(0, pos) + box.subList(pos + 1, box.size)).toMutableList()
+            }
+        }
+    }
+    var result = 0
+    boxes.forEach { box ->
+        box.value.forEachIndexed { idx, item ->
+            result += (box.key + 1) * (idx + 1) * item.second
+        }
+    }
+    return result
 }
 
 private fun calcHash(str: String): Int {
@@ -20,9 +56,6 @@ private fun readInput(testCase: String): List<String> {
     val reader =
         object {}.javaClass.getResourceAsStream(testCase)?.bufferedReader()
             ?: throw Exception("Invalid state, cannot read the input")
-    while (true) {
-        val rawLine = (reader.readLine() ?: break).trim()
-        return rawLine.split(',')
-    }
-    return emptyList()
+    val rawLine = (reader.readLine() ?: return emptyList()).trim()
+    return rawLine.split(',')
 }
