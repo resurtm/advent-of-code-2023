89c09042c6dbd4ee1c1fa5507843542bba2546be
resurtm <resurtm@gmail.com>
2023-12-17 09:00:22 +0100

    Day 17, part 1, wip

--- app/src/main/kotlin/com/resurtm/aoc2023/day17/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day17/Solution.kt
@@ -1,8 +1,67 @@
 package com.resurtm.aoc2023.day17
 
 fun launchDay17(testCase: String) {
-    val grid = readInputGrid(testCase)
-    printGrid(grid)
+    traverse(Pos(), readInputGrid(testCase))
+}
+
+private fun traverse(startPos: Pos, gr: Grid) {
+    var paths = mutableListOf(Path(mutableListOf(startPos)))
+
+    var genCount = 0
+    while (genCount < 4) {
+        val newPaths = mutableListOf<Path>()
+        for (path in paths) {
+            val pos = path.items.last()
+            val toAdd = mutableListOf<Pos>()
+
+            if (pos.row > 0) {
+                val tmpPos = pos.copy(row = pos.row - 1)
+                if (!path.items.contains(tmpPos) && !inLine(path.items + listOf(tmpPos)))
+                    toAdd.add(tmpPos)
+            }
+            if (pos.row < gr.size - 2) {
+                val tmpPos = pos.copy(row = pos.row + 1)
+                if (!path.items.contains(tmpPos) && !inLine(path.items + listOf(tmpPos)))
+                    toAdd.add(tmpPos)
+            }
+            if (pos.col > 0) {
+                val tmpPos = pos.copy(col = pos.col - 1)
+                if (!path.items.contains(tmpPos) && !inLine(path.items + listOf(tmpPos)))
+                    toAdd.add(tmpPos)
+            }
+            if (pos.col < gr[0].size - 2) {
+                val tmpPos = pos.copy(col = pos.col + 1)
+                if (!path.items.contains(tmpPos) && !inLine(path.items + listOf(tmpPos)))
+                    toAdd.add(tmpPos)
+            }
+
+            toAdd.forEach {
+                val items = (path.items + it).toMutableList()
+                val sum = path.sum + gr[it.row][it.col]
+                newPaths.add(Path(items, sum))
+            }
+        }
+        paths = newPaths
+
+        genCount++
+    }
+}
+
+private fun inLine(lst: List<Pos>): Boolean {
+    if (lst.size < 4) return false
+    var vertical = true
+    var horizontal = true
+    for (item in lst.subList(lst.size - 3, lst.size))
+        if (item.col != lst[lst.size - 3].col) {
+            vertical = false
+            break
+        }
+    for (item in lst.subList(lst.size - 3, lst.size))
+        if (item.row != lst[lst.size - 3].row) {
+            horizontal = false
+            break
+        }
+    return vertical || horizontal
 }
 
 private fun printGrid(gr: Grid) {
@@ -25,3 +84,7 @@ private fun readInputGrid(testCase: String): Grid {
 }
 
 private typealias Grid = List<List<Int>>
+
+private data class Pos(val row: Int = 0, val col: Int = 0)
+
+private data class Path(val items: MutableList<Pos> = mutableListOf(), val sum: Int = 0)
