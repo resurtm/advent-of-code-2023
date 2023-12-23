1fed6f67a398eb4ffca501a46906e8c15733d250
resurtm <resurtm@gmail.com>
2023-12-23 10:28:41 +0100

    Day 23, part 1, final

--- app/src/main/kotlin/com/resurtm/aoc2023/day23/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day23/Solution.kt
@@ -1,18 +1,82 @@
 package com.resurtm.aoc2023.day23
 
 fun launchDay23(testCase: String) {
-    val grid = Grid.readInput(testCase)
-    grid.print()
-    println(grid.start)
-    println(grid.end)
+    println("Day 23, part 1: ${Grid.readInput(testCase).solvePart1()}")
 }
 
-private data class Grid(val grid: List<List<Char>>, val start: Pos, val end: Pos) {
+private data class Grid(
+    val grid: List<List<Node>>,
+    val start: Pos,
+    val end: Pos
+) {
+    fun solvePart1(): Int {
+        val paths = mutableListOf(mutableListOf(start.copy()))
+        var maxSize = 0
+
+        while (paths.size > 0) {
+            var pathIdx = 0
+            while (pathIdx < paths.size) {
+                val nexts = findNextPositions(paths[pathIdx])
+                if (nexts.isEmpty()) {
+                    if (maxSize < paths[pathIdx].size)
+                        maxSize = paths[pathIdx].size
+                    paths.removeAt(pathIdx)
+                    continue
+                }
+
+                for (nextIdx in nexts.indices) {
+                    if (nextIdx == 0) {
+                        paths[pathIdx].add(nexts[0])
+                    } else {
+                        val copied = paths[pathIdx].map { it.copy() }
+                        copied.removeLast()
+                        paths.add((copied + nexts[nextIdx]).toMutableList())
+                    }
+                }
+
+                pathIdx++
+            }
+        }
+
+        return maxSize - 1
+    }
+
+    fun findNextPositions(visited: List<Pos>, posInp: Pos? = null): List<Pos> {
+        val pos = posInp ?: visited.last()
+        val freeChs = arrayOf('.', '^', '>', 'v', '<')
+        val directions = arrayOf(
+            Pair(Pos(-1L, 0L), 'v'),
+            Pair(Pos(1L, 0L), '^'),
+            Pair(Pos(0L, -1L), '>'),
+            Pair(Pos(0L, 1L), '<'),
+        )
+        return directions
+            .asSequence()
+            .map {
+                val p = it.first
+                Pair(
+                    it.first.copy(row = pos.row + p.row, col = pos.col + p.col),
+                    it.second
+                )
+            }
+            .filter {
+                val p = it.first
+                p.row >= 0 && p.col >= 0 && p.row <= grid.size - 1 && p.col <= grid[0].size - 1
+            }
+            .filter {
+                val ch = grid[it.first.row.toInt()][it.first.col.toInt()].ch
+                ch != it.second && ch in freeChs
+            }
+            .map { it.first }
+            .filter { visited.indexOf(it) == -1 }
+            .toList()
+    }
+
     fun print() {
         println("----------")
         for (row in grid.indices) {
             for (col in grid[row].indices)
-                print(grid[row][col])
+                print(grid[row][col].ch)
             println()
         }
     }
@@ -23,23 +87,24 @@ private data class Grid(val grid: List<List<Char>>, val start: Pos, val end: Pos
                 object {}.javaClass.getResourceAsStream(testCase)?.bufferedReader()
                     ?: throw Exception("Cannot read an input, probably it is invalid")
 
-            val grid = mutableListOf<List<Char>>()
+            val grid = mutableListOf<List<Node>>()
             while (true) {
                 val rawLine = reader.readLine()
                     ?: break
                 if (rawLine.trim().isEmpty())
                     continue
-                grid.add(rawLine.trim().toList())
+                val row = rawLine.trim().toList().map { Node(it) }
+                grid.add(row)
             }
 
             val lastRow = grid.size - 1
-            return Grid(
-                grid,
-                start = Pos(row = 0, col = grid[0].indexOf('.').toLong()),
-                end = Pos(row = lastRow.toLong(), col = grid[lastRow].indexOf('.').toLong())
-            )
+            val start = Pos(row = 0, col = grid[0].indexOfFirst { it.ch == '.' }.toLong())
+            val end = Pos(row = lastRow.toLong(), col = grid[lastRow].indexOfFirst { it.ch == '.' }.toLong())
+            return Grid(grid, start, end)
         }
     }
 }
+
+private data class Node(val ch: Char)
 
 private data class Pos(val row: Long, val col: Long)
