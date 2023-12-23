664d8f5cd92c08b3fb016fce479c3f8b313896b4
resurtm <resurtm@gmail.com>
2023-12-23 13:47:54 +0100

    Day 23, part 2, wip

--- app/src/main/kotlin/com/resurtm/aoc2023/day23/Solution.kt
+++ app/src/main/kotlin/com/resurtm/aoc2023/day23/Solution.kt
@@ -14,17 +14,17 @@ internal data class Grid(
     fun solvePart2Graph(): Int {
         val queue = ArrayDeque<Pos>()
         queue.add(start)
-        grid[start.row.toInt()][start.col.toInt()].dist = 0L
+        grid[start.row][start.col].dist = 0
 
         while (queue.isNotEmpty()) {
             val currPos = queue.removeFirstOrNull() ?: break
-            val currNode = grid[currPos.row.toInt()][currPos.col.toInt()]
+            val currNode = grid[currPos.row][currPos.col]
             currNode.visited = true
 
             val nexts = findNextPositionsV2(currPos)
-            if (currNode.dist != Long.MIN_VALUE) {
+            if (currNode.dist != Int.MIN_VALUE) {
                 nexts.forEach {
-                    val otherNode = grid[it.row.toInt()][it.col.toInt()]
+                    val otherNode = grid[it.row][it.col]
                     if (otherNode.dist < currNode.dist + 1)
                         otherNode.dist = currNode.dist + 1
                 }
@@ -32,14 +32,14 @@ internal data class Grid(
             queue.addAll(nexts)
         }
 
-        var maxDist = Long.MIN_VALUE
+        var maxDist = Int.MIN_VALUE
         grid.forEach { row ->
             row.forEach {
                 if (maxDist < it.dist)
                     maxDist = it.dist
             }
         }
-        return maxDist.toInt()
+        return maxDist
     }
 
     fun solvePart2(): Int {
@@ -60,12 +60,15 @@ internal data class Grid(
             while (pathIdx < paths.size) {
                 val path = paths[pathIdx]
 
-                val nexts = findNextPositionsV1(path[path.size - 1], path, ignoreDirs)
+                // val nexts = findNextPositionsV1(path[path.size - 1], path, ignoreDirs)
+                val nexts = findNextPositionsV3(path[path.size - 1], path)
+
                 if (nexts.isEmpty()) {
                     if (maxSize < path.size && path.last() == end) {
                         maxSize = path.size
                         maxPath = path
                     }
+                    // println(path)
                     paths.removeAt(pathIdx)
                     continue
                 }
@@ -92,12 +95,64 @@ internal data class Grid(
         .map { it.copy(row = pos.row + it.row, col = pos.col + it.col) }
         .filter { it.row >= 0L && it.col >= 0L && it.row <= grid.size - 1L && it.col <= grid[0].size - 1L }
         .filter {
-            val node = grid[it.row.toInt()][it.col.toInt()]
-            node.ch in freeChs && !node.visited
+            val node = grid[it.row][it.col]
+            node.ch in emptyChars && !node.visited
         }
         .toList()
 
-    private fun findNextPositionsV1(pos: Pos, exclude: List<Pos>, ignoreDirs: Boolean): List<Pos> = directions
+    internal fun findNextPositionsV3(
+        p: Pos,
+        exclude: List<Pos> = emptyList(),
+    ): List<Pos> {
+        val res = mutableListOf<Pos>()
+        for (row in p.row + 1..<grid.size) {
+            if (grid[row][p.col].ch !in emptyChars) {
+                if (Pos(row - 1, p.col) != p)
+                    res.add(Pos(row - 1, p.col))
+                break
+            }
+            if (row == grid.size - 1) {
+                res.add(Pos(row, p.col))
+            }
+        }
+        for (row in p.row - 1 downTo 0) {
+            if (grid[row][p.col].ch !in emptyChars) {
+                if (Pos(row + 1, p.col) != p)
+                    res.add(Pos(row + 1, p.col))
+                break
+            }
+            if (row == 0) {
+                res.add(Pos(row, p.col))
+            }
+        }
+        for (col in p.col + 1..<grid[0].size) {
+            if (grid[p.row][col].ch !in emptyChars) {
+                if (Pos(p.row, col - 1) != p)
+                    res.add(Pos(p.row, col - 1))
+                break
+            }
+            if (col == grid[0].size - 1) {
+                res.add(Pos(p.row, col))
+            }
+        }
+        for (col in p.col - 1 downTo 0) {
+            if (grid[p.row][col].ch !in emptyChars) {
+                if (Pos(p.row, col + 1) != p)
+                    res.add(Pos(p.row, col + 1))
+                break
+            }
+            if (col == 0) {
+                res.add(Pos(p.row, col))
+            }
+        }
+        return res.filter { !exclude.contains(it) }
+    }
+
+    private fun findNextPositionsV1(
+        pos: Pos,
+        exclude: List<Pos> = emptyList(),
+        ignoreDirs: Boolean = false,
+    ): List<Pos> = directions
         .asSequence()
         .map {
             val p = it.first
@@ -111,8 +166,8 @@ internal data class Grid(
             p.row >= 0L && p.col >= 0L && p.row <= grid.size - 1L && p.col <= grid[0].size - 1L
         }
         .filter {
-            val ch = grid[it.first.row.toInt()][it.first.col.toInt()].ch
-            if (ignoreDirs) ch in freeChs else ch != it.second && ch in freeChs
+            val ch = grid[it.first.row][it.first.col].ch
+            if (ignoreDirs) ch in emptyChars else ch != it.second && ch in emptyChars
         }
         .filter { it.first !in exclude }
         .map { it.first }
@@ -122,21 +177,20 @@ internal data class Grid(
         println("----------")
         for (row in grid.indices) {
             for (col in grid[row].indices) {
-                val pos = Pos(row.toLong(), col.toLong())
-                print(if (maxPath.contains(pos)) 'O' else grid[row][col].ch)
+                print(if (maxPath.contains(Pos(row, col))) 'O' else grid[row][col].ch)
             }
             println()
         }
     }
 
     companion object {
-        private val freeChs = arrayOf('.', '^', '>', 'v', '<')
+        private val emptyChars = arrayOf('.', '^', '>', 'v', '<')
 
         private val directions = arrayOf(
-            Pair(Pos(-1L, 0L), 'v'),
-            Pair(Pos(1L, 0L), '^'),
-            Pair(Pos(0L, -1L), '>'),
-            Pair(Pos(0L, 1L), '<'),
+            Pair(Pos(-1, 0), 'v'),
+            Pair(Pos(1, 0), '^'),
+            Pair(Pos(0, -1), '>'),
+            Pair(Pos(0, 1), '<'),
         )
 
         internal fun readInput(testCase: String): Grid {
@@ -155,13 +209,13 @@ internal data class Grid(
             }
 
             val lastRow = grid.size - 1
-            val start = Pos(row = 0L, col = grid[0].indexOfFirst { it.ch == '.' }.toLong())
-            val end = Pos(row = lastRow.toLong(), col = grid[lastRow].indexOfFirst { it.ch == '.' }.toLong())
+            val start = Pos(row = 0, col = grid[0].indexOfFirst { it.ch == '.' })
+            val end = Pos(row = lastRow, col = grid[lastRow].indexOfFirst { it.ch == '.' })
             return Grid(grid, start, end)
         }
     }
 }
 
-internal data class Node(val ch: Char, var dist: Long = Long.MIN_VALUE, var visited: Boolean = false)
+internal data class Node(val ch: Char, var dist: Int = Int.MIN_VALUE, var visited: Boolean = false)
 
-internal data class Pos(val row: Long, val col: Long)
+internal data class Pos(val row: Int, val col: Int)
--- app/src/test/kotlin/com/resurtm/aoc2023/Day23Test.kt
+++ app/src/test/kotlin/com/resurtm/aoc2023/Day23Test.kt
@@ -7,14 +7,23 @@ class Day23Test {
     @Test
     fun canSolvePart1() {
         val expected = 94
-        val actual = Grid.readInput("/day23/test-case.txt").solvePart1()
+        val actual = grid().solvePart1()
         assertEquals(expected, actual)
     }
 
     @Test
     fun canSolvePart2() {
         val expected = 154
-        val actual = Grid.readInput("/day23/test-case.txt").solvePart2()
+        val actual = grid().solvePart2()
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun testFindNextPositionsV3() {
+        val expected = listOf(Pos(17, 13), Pos(19, 15), Pos(19, 11))
+        val actual = grid().findNextPositionsV3(Pos(19, 13))
+        assertEquals(expected, actual)
+    }
+
+    private fun grid() = Grid.readInput("/day23/test-case.txt")
 }
